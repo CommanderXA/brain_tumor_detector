Zero Gradient fix

src/dataset.py
@@ -1,7 +1,11 @@
 import os
+
 import pandas as pd
 from PIL import Image
+import torch
 from torch.utils.data import Dataset
+
+from utils import label_to_num
 
 
 class MRIDataset(Dataset):
@@ -25,7 +29,7 @@ class MRIDataset(Dataset):
     def __getitem__(self, index: int) -> any:
         data_path = os.path.join(self.root_dir, self.labels.iloc[index, 1])
         image = Image.open(data_path)
-        label = self.labels.iloc[index, 2]
+        label = torch.Tensor(label_to_num(self.labels.iloc[index, 2]))
         if self.transform:
             image = self.transform(image)
         return image, label

src/main.py
@@ -6,8 +6,8 @@ import hydra
 from omegaconf import DictConfig
 
 import torch
-from torch.nn import BCELoss
-from torch.optim import SGD
+from torch.nn import MSELoss
+from torch.optim import Adam
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
@@ -23,15 +23,16 @@ def main(cfg: DictConfig):
 
     # setup
     log = logging.getLogger(__name__)
-    Config.set_device()
-    Config.set_cfg(cfg)
-    Config.set_log(log)
+    Config.setup(cfg, log)
     Config.log.info(f"Performing setup")
     model = Model().to(Config.device)
-    criterion = BCELoss()
-    optimizer = SGD(model.parameters(), lr=Config.cfg.hyperparams.lr)
+    Config.log.info(
+        "Model has: " + str(model.parameters_count()) + " parameters")
+    criterion = MSELoss()
+    optimizer = Adam(model.parameters(), lr=Config.cfg.hyperparams.lr)
 
     if Config.cfg.core.train == True:
+
         # datasets
         Config.log.info(f"Loading Dataset")
         train_dataset = MRIDataset(
@@ -42,8 +43,8 @@ def main(cfg: DictConfig):
             root_dir=Config.cfg.files.data_dir,
             transform=transforms.Compose([
                 transforms.Grayscale(),
+                transforms.Resize((256, 256)),
                 transforms.ToTensor(),
-                transforms.Resize((256, 256))
             ])
         )
         val_dataset = MRIDataset(
@@ -54,8 +55,8 @@ def main(cfg: DictConfig):
             root_dir=Config.cfg.files.data_dir,
             transform=transforms.Compose([
                 transforms.Grayscale(),
+                transforms.Resize((256, 256)),
                 transforms.ToTensor(),
-                transforms.Resize((256, 256))
             ])
         )
         test_dataset = MRIDataset(
@@ -66,16 +67,16 @@ def main(cfg: DictConfig):
             root_dir=Config.cfg.files.data_dir,
             transform=transforms.Compose([
                 transforms.Grayscale(),
+                transforms.Resize((256, 256)),
                 transforms.ToTensor(),
-                transforms.Resize((256, 256))
             ])
         )
 
         # dataloaders
         train_dataloader = DataLoader(
-            train_dataset, batch_size=1, shuffle=True, num_workers=3)
+            train_dataset, batch_size=Config.cfg.hyperparams.batch_size, shuffle=True, num_workers=3)
         val_dataloader = DataLoader(
-            val_dataset, batch_size=1, shuffle=True, num_workers=3)
+            val_dataset, batch_size=Config.cfg.hyperparams.batch_size, shuffle=True, num_workers=3)
 
         # training
         Config.log.info(f"Starting training")
@@ -99,7 +100,7 @@ def main(cfg: DictConfig):
 
     if Config.cfg.core.inference is not False:
         prediction = forward(model)
-        Config.log.critical(
+        Config.log.info(
             f"Image {Config.cfg.core.inference} is classified as: {prediction}")
 
 

src/model.py
@@ -2,16 +2,20 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+from config import Config
+
 
 class Model(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.conv2d = Conv2dBlock(1, 64)
-        self.conv2d_1 = Conv2dBlock(64, 128)
-        self.conv2d_2 = Conv2dBlock(128, 128)
+        self.conv2d = Conv2dBlock(1, 32)
+        self.conv2d_1 = Conv2dBlock(32, 64)
+        self.conv2d_2 = Conv2dBlock(64, 128)
         self.conv2d_3 = Conv2dBlock(128, 128)
-        self.conv2d_4 = Conv2dBlock(128, 256)
-        self.lin = nn.Linear(16384, 256)
+        self.conv2d_4 = Conv2dBlock(64, 128)
+        self.batch_norm = nn.BatchNorm2d(64)
+        self.batch_norm_2 = nn.BatchNorm2d(128)
+        self.lin = nn.Linear(131072, 256)
         self.lin_1 = nn.Linear(256, 128)
         self.lin_2 = nn.Linear(128, 64)
         self.lin_3 = nn.Linear(64, 1)
@@ -19,10 +23,12 @@ class Model(nn.Module):
     def forward(self, x) -> torch.Tensor:
         x = self.conv2d(x)
         x = self.conv2d_1(x)
-        x = self.conv2d_2(x)
-        x = self.conv2d_3(x)
+        x = self.batch_norm(x)
+        # x = self.conv2d_2(x)
+        # x = self.conv2d_3(x)
         x = self.conv2d_4(x)
-        x = x.reshape(1, -1)
+        x = self.batch_norm_2(x)
+        x = x.reshape(-1, 131072)
         x = F.relu(self.lin(x))
         x = F.relu(self.lin_1(x))
         x = F.relu(self.lin_2(x))

src/train.py
@@ -6,7 +6,6 @@ from tqdm import tqdm
 
 from config import Config
 from model import Model
-from utils import label_to_num, prediction_to_class
 
 
 def train(
@@ -18,30 +17,35 @@ def train(
     optimizer: optim.Adam
 ):
     for epoch in range(epochs):
+        epoch_loss = 0
         correct = 0
+        total = 0
+
         model.train()
         with tqdm(dataloader) as epoch_data:
             for sample, target in epoch_data:
                 epoch_data.set_description(f"Epoch: {epoch}/{epochs}")
 
                 sample = sample.to(Config.device)
-                target = label_to_num(target).to(Config.device)
+                target = target.to(Config.device)
 
                 out = model(sample)
-                if (prediction_to_class(out) == target).item():
-                    correct += 1
+                total += target.size(0)
+                correct += (torch.round(out) == target).sum().item()
 
                 # compute loss
                 loss = criterion(out, target)
                 loss.backward()
+                epoch_loss += loss.item()/len(dataloader)
 
                 # optimize
-                optimizer.zero_grad()
                 optimizer.step()
+                optimizer.zero_grad()
 
                 epoch_data.set_postfix(
-                    loss=loss.item(), accuracy=correct/len(dataloader)*100.)
+                    loss=loss.item(), accuracy=f"{(correct/total*100.):.2f}%")
 
+        total = 0
         with torch.no_grad():
             val_correct = 0
             model.eval()
@@ -50,14 +54,15 @@ def train(
                     epoch_val_data.set_description(f"Validation: ")
 
                     sample = sample.to(Config.device)
-                    target = label_to_num(target).to(Config.device)
+                    target = target.to(Config.device)
 
                     out = model(sample)
-                    if prediction_to_class(out) == target:
-                        val_correct += 1
+
+                    total += target.size(0)
+                    val_correct += (torch.round(out) == target).sum().item()
 
                     # compute loss
                     loss = criterion(out, target)
 
-                    epoch_val_data.set_postfix(
-                        loss=loss.item(), accuracy=val_correct/len(val_dataloader)*100.)
+                    epoch_data.set_postfix(
+                        loss=loss.item(), accuracy=f"{(val_correct/total*100.):.2f}%")